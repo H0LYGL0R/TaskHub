f749cb0860832476a91ca66e39fbd6374e741e77
v_yashchik <medyantsevsvyt@gmail.com>
2026-03-02 00:54:27 +0500

    feat: student info middleware implemented

---
+++ TaskHub/Api/Middlewares/StudentInfoMiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace Api.Middlewares;
+
+public sealed class StudentInfoMiddleware
+{
+    private const string StudentName = "Medyantesev Svyatoslav Victorovich";
+    private const string StudentGroup = "RI-240943";
+
+    private readonly RequestDelegate _next;
+
+    public StudentInfoMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Response.OnStarting(() =>
+        {
+            context.Response.Headers["X-Student-Name"] = StudentName;
+            context.Response.Headers["X-Student-Group"] = StudentGroup;
+
+            return Task.CompletedTask;
+        });
+
+        await _next(context);
+    }
+}
