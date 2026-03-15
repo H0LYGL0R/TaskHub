d17801405c98491e8e24b571f7330d6ae181b132
v_yashchik <medyantsevsvyt@gmail.com>
2026-03-16 00:59:07 +0500

    feat: ValidateUserRequestAttribute implemented

---
+++ TaskHub/Api/Attributes/ValidateUserRequestAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class ValidateUserRequestAttribute : Attribute, IActionFilter
+{
+    private const string RequestMissingMessage = "Тело запроса отсутствует";
+    private const string NameMissingMessage = "Имя пользователя не задано";
+    private const string NameProperty = "Name";
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        object? request = 
+            context.ActionArguments.Values.FirstOrDefault(arg => arg is not null);
+
+        if (request is null)
+        {
+            context.Result = new BadRequestObjectResult(RequestMissingMessage);
+            return;
+        }
+
+        PropertyInfo? nameProperty = request.GetType().GetProperty(NameProperty);
+        string? name = nameProperty?.GetValue(request) as string;
+
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            context.Result = new BadRequestObjectResult(NameMissingMessage);
+        }
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+    }
+}
