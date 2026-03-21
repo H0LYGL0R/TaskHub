17bc2b62177b08b7504b0b727650624cb44fffbe
v_yashchik <medyantsevsvyt@gmail.com>
2026-03-21 21:11:23 +0500

    feat: ManageTaskUseCase & TaskService interfaces implemented

---
+++ TaskHub/Api/Services/TaskService/ITaskService.cs
@@ -0,0 +1,21 @@
+﻿using Api.Tasks.Models;
+
+namespace Api.Services.TaskService;
+
+/// <summary>
+/// Сервис задач
+/// </summary>
+public interface ITaskService
+{
+    Task<TaskModel?> CreateTaskAsync(string title, Guid createdByUserId, CancellationToken cancellationToken);
+
+    Task<IReadOnlyCollection<TaskModel>> GetAllTasksAsync(CancellationToken cancellationToken);
+
+    Task<TaskModel?> GetTaskByIdAsync(Guid taskId, CancellationToken cancellationToken);
+
+    Task<bool> SetTaskTitleAsync(Guid taskId, string title, CancellationToken cancellationToken);
+
+    Task<bool> DeleteTaskByIdAsync(Guid taskId, CancellationToken cancellationToken);
+
+    Task DeleteAllTasksAsync(CancellationToken cancellationToken);
+}
