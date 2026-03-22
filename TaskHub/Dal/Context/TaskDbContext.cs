4be62e70b1c8d179ee88a6b10cef9a4f7d8ea111
v_yashchik <medyantsevsvyt@gmail.com>
2026-03-22 21:57:59 +0500

    db: task added

---
+++ TaskHub/Dal/Context/TaskDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Dal.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal.Context;
+
+public sealed class TaskDbContext(DbContextOptions<TaskDbContext> options) : DbContext(options)
+{
+    public DbSet<TaskEntity> Tasks => Set<TaskEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("users");
+            entity.HasKey(x => x.Id);
+        });
+
+        modelBuilder.Entity<TaskEntity>(entity =>
+        {
+            entity.ToTable("tasks");
+
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.Title)
+                .HasColumnName("title")
+                .HasMaxLength(500);
+
+            entity.Property(x => x.CreatedByUserId)
+                .HasColumnName("created_by_user_id")
+                .IsRequired();
+
+            entity.Property(x => x.CreatedUtc)
+                .HasColumnName("created_utc")
+                .IsRequired();
+
+            entity.HasOne(x => x.CreatedByUser)
+                .WithMany()
+                .HasForeignKey(x => x.CreatedByUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
