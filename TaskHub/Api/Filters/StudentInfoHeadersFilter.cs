9347ce3e2c3ae6bb2caa658d6da6bd8f0826f138
v_yashchik <medyantsevsvyt@gmail.com>
2026-04-05 22:02:55 +0500

    feat: StudentInfoHeadersFilter implemented

---
+++ TaskHub/Api/Filters/StudentInfoHeadersFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Filters;
+
+public sealed class StudentInfoHeadersFilter : IAsyncResultFilter
+{
+    private const string StudentName = "Medyantsev Svyatoslav Victorovich";
+    private const string StudentGroup = "RI-240943";
+
+    public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        context.HttpContext.Response.Headers["X-Student-Name"] = StudentName;
+        context.HttpContext.Response.Headers["X-Student-Group"] = StudentGroup;
+
+        await next();
+    }
+}
