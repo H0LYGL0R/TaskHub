7ef0e3c3c4a5dbe8c112e72c2a59f80226f0a7e1
v_yashchik <medyantsevsvyt@gmail.com>
2026-04-05 16:24:49 +0500

    feat: FromRouteTaskIdAttribute implemented

---
+++ TaskHub/Api/Attributes/FromRouteTaskIdAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Api.Attributes;
+
+[AttributeUsage(AttributeTargets.Parameter)]
+public sealed class FromRouteTaskIdAttribute : ModelBinderAttribute
+{
+    public FromRouteTaskIdAttribute()
+    {
+        BinderType = null;
+        BindingSource = BindingSource.Path;
+    }
+}
