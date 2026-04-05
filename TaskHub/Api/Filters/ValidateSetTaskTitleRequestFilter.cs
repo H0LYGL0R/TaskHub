9c95d41ed4f5e1ff1991b54cbf24306fa23899c3
v_yashchik <medyantsevsvyt@gmail.com>
2026-04-05 22:20:21 +0500

    feat: ValidateSetTaskTitleRequestFilter implemented

---
+++ TaskHub/Api/Filters/ValidateSetTaskTitleRequestFilter.cs
@@ -0,0 +1,23 @@
+﻿using Api.Tasks.Requests;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Filters;
+
+public sealed class ValidateSetTaskTitleRequestFilter : IActionFilter
+{
+    private const string RequestMissingMessage = "Тело запроса отсутствует";
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        SetTaskTitleRequest? request = context.ActionArguments.Values.OfType<SetTaskTitleRequest?>().FirstOrDefault();
+        if (request is null)
+        {
+            context.Result = new BadRequestObjectResult(RequestMissingMessage);
+        }
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+    }
+}
